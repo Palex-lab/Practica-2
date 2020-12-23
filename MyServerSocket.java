441291bf06db58eb23c52f1e0ce5380bf724c02f
Palex-lab <71607147+Palex-lab@users.noreply.github.com>
2020-12-23 12:41:43 +0100

    New class MyServerSocket

--- MyServerSocket.java
+++ MyServerSocket.java
@@ -1,28 +1,79 @@
+package Sockets;
+
+import Sockets.MySocket;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.channels.ServerSocketChannel;
 
 public class MyServerSocket extends ServerSocket {
 
-    MySocket serverSocket;
 
     public MyServerSocket(int port) throws IOException {
         super(port);
     }
-    
-    public MySocket accept(){
-        try {
-            serverSocket = new MySocket(super.accept());
-            return serverSocket;
-        } catch (IOException e) {
+
+    //Listens for a connection to be made to this socket and accepts it.
+    public Socket accept(){
+        try{
+            return super.accept();
+        }catch(IOException e){
+            e.printStackTrace();
             return null;
         }
     }
-
+    public MySocket myAccept(){
+        try{
+            Socket socket = super.accept();
+		    return new MySocket(socket);
+        }catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    //Close the current Server Socket
     public void close(){
-        try {
+        try{
             super.close();
-        } catch (IOException e) {
+        }catch(IOException e){
             e.printStackTrace();
         }
     }
+    /*Binds the socket to a specific address (IP+PORT)*/
+    public void bind(SocketAddress endpoint, int backlog){
+        try{
+            super.bind(endpoint, backlog);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public ServerSocketChannel getChannel(){
+        return super.getChannel();
+    }
+
+    public InetAddress getInetAddress(){
+            return super.getInetAddress();
+    }
+    public boolean isClosed(){
+        return super.isClosed();
+    }
+    public void setReceiveBufferSize(int size){
+        try{
+            super.setReceiveBufferSize(size);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void setSoTimeout(int timeout){
+        try{
+            super.setSoTimeout(timeout);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
 }
